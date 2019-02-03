415d42d8d0676219088a9110efda813553cdf740
Yogeshwarathe <43827580+Yogeshwarathe@users.noreply.github.com>
2019-02-04 00:18:30 +0530

    Add files via upload

---
+++ words.py
@@ -0,0 +1,24 @@
+import string
+import random
+
+def load_words():
+    """
+    Ye function kaafi jayada words ko load karne mai help karega
+    """
+    inFile = open("word.txt",'r')
+    line = inFile.readline()
+    word_list = string.split(line)
+    print "  ", len(word_list), "words loaded.\n"
+
+    return word_list
+
+def choose_word():
+    """
+    word_list (list): list of words (strings)
+    ye function ek word randomly return karega
+    """
+    word_list = load_words()
+    secret_word = random.choice(word_list)
+    secret_word = secret_word.lower()
+
+    return secret_word
